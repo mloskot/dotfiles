957698bbe8f051ecaa7cf1df2b3cf6b439381832
Mateusz Loskot <mateusz@loskot.net>
2018-05-13 23:35:17 +0200

    Rename cmake installer. Update to install cmake 3.11.1

--- scripts/install-cmake-latest.sh
+++ scripts/install-cmake-latest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -x
 
-wget https://cmake.org/files/v3.11/cmake-3.11.0-Linux-x86_64.sh
+wget https://cmake.org/files/v3.11/cmake-3.11.1-Linux-x86_64.sh
 sudo sh cmake-3.11.0-Linux-x86_64.sh -- --skip-license --prefix=/usr/local
