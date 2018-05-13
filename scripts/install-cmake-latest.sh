054a7d72a5e76f63d7071c4b64f1f0b223cf8229
Mateusz Loskot <mateusz@loskot.net>
2018-05-13 23:43:30 +0200

    Fix cmake installer name

--- scripts/install-cmake-latest.sh
+++ scripts/install-cmake-latest.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -x
 
-wget https://cmake.org/files/v3.11/cmake-3.11.1-Linux-x86_64.sh
-sudo sh cmake-3.11.0-Linux-x86_64.sh -- --skip-license --prefix=/usr/local
+wget -O cmake-linux.sh https://cmake.org/files/v3.11/cmake-3.11.1-Linux-x86_64.sh
+sudo sh cmake-linux.sh -- --skip-license --prefix=/usr/local
+cmake --version
