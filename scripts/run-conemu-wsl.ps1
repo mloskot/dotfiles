ac24e32604e39f21e4e07e209ee61271739f39b7
Mateusz Loskot <mateusz@loskot.net>
2018-05-23 11:23:46 +0200

    Add scratch notes of PowerShell script to run ConEmu with WSL
    https://github.com/Microsoft/WSL/issues/2578

---
+++ scripts/run-conemu-wsl.ps1
@@ -0,0 +1,5 @@
+# TODO
+
+# Get-ChildItem "HKCU:\Software\Microsoft\Windows\CurrentVersion\Lxss" -Recurse
+#
+# set "PATH=%ConEmuBaseDirShort%\wsl;%PATH%" & %ConEmuBaseDirShort%\conemu-cyg-64.exe --wsl --distro-guid={bc996e25-168f-4375-bf3b-afbf564265b3} -cur_console:pm:/mnt/d
