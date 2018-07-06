0945b512d61dcabb2eedcc7fed87efb26e7bd32d
Mateusz Łoskot <mateusz@loskot.net>
2018-07-06 19:04:50 +0200

    Update CMake to 3.12 RC2

--- wsl/scripts/install-cmake-latest.sh
+++ wsl/scripts/install-cmake-latest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x
 
-wget -O cmake-linux.sh https://cmake.org/files/v3.11/cmake-3.11.2-Linux-x86_64.sh
+wget -O cmake-linux.sh https://cmake.org/files/v3.12/cmake-3.12.0-rc2-Linux-x86_64.sh
 sudo sh cmake-linux.sh -- --skip-license --prefix=/usr/local
 cmake --version
