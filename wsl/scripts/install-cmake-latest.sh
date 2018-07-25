eb0c8c63435a985604492809c9f33869ca3f49c6
Mateusz Łoskot <mateusz@loskot.net>
2018-07-25 13:49:11 +0200

    Update CMake to 3.12. Add uninstall commands.

--- wsl/scripts/install-cmake-latest.sh
+++ wsl/scripts/install-cmake-latest.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -x
 
-wget -O cmake-linux.sh https://cmake.org/files/v3.12/cmake-3.12.0-rc2-Linux-x86_64.sh
+sudo rm -rf /usr/local/bin/cmake*
+sudo rm -rf /usr/local/bin/ctest*
+sudo rm -rf /usr/local/doc/cmake
+sudo rm -rf /usr/local/man/man1/cmake*
+sudo rm -rf /usr/local/man/man1/ctest*
+sudo rm -rf /usr/local/man/man7/cmake*
+sudo rm -rf /usr/local/share/cmake*
+
+wget -O cmake-linux.sh https://cmake.org/files/v3.12/cmake-3.12.0-Linux-x86_64.sh
 sudo sh cmake-linux.sh -- --skip-license --prefix=/usr/local
 cmake --version
