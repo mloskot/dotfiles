ee4286dd5d297e2b9ce4db81cee569d6ba0a5529
Mateusz Łoskot <mateusz@loskot.net>
2018-07-31 18:32:12 +0200

    Run checkout master and pull

--- win/scripts/boost-superproject-start-over.bat
+++ win/scripts/boost-superproject-start-over.bat
@@ -8,4 +8,6 @@ git clean -xfd
 git submodule foreach --recursive git clean -xfd
 git reset --hard
 git submodule foreach --recursive git reset --hard
+git checkout master
+git pull
 git submodule update --init --recursive --jobs 8
