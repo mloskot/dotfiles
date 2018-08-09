a35092c137965210810f19fddbba9454b9888648
Mateusz Łoskot <mateusz@loskot.net>
2018-08-09 20:04:04 +0200

    Update CMake to 3.12.1

--- wsl/scripts/install-cmake-latest.sh
+++ wsl/scripts/install-cmake-latest.sh
@@ -9,6 +9,6 @@ sudo rm -rf /usr/local/man/man1/ctest*
 sudo rm -rf /usr/local/man/man7/cmake*
 sudo rm -rf /usr/local/share/cmake*
 
-wget -O cmake-linux.sh https://cmake.org/files/v3.12/cmake-3.12.0-Linux-x86_64.sh
+wget -O cmake-linux.sh https://cmake.org/files/v3.12/cmake-3.12.1-Linux-x86_64.sh
 sudo sh cmake-linux.sh -- --skip-license --prefix=/usr/local
 cmake --version
