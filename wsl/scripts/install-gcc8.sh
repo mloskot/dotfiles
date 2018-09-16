19fba0c6de6f26ff2c0679641ab96da58c6214e8
Mateusz Łoskot <mateusz@loskot.net>
2018-09-16 21:46:54 +0200

    Add install-gcc8.sh

---
+++ wsl/scripts/install-gcc8.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -x
+
+if type lsb_release >/dev/null 2>&1; then
+    if lsb_release -sc | grep bionic; then
+        echo "Ubuntu Bionic with native GCC 8 packages"
+    else
+        sudo add-apt-repository ppa:ubuntu-toolchain-r/test   # latest GCC
+    fi
+fi
+
+sudo apt-get update
+sudo apt-get install gcc-8 g++-8
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 60 --slave /usr/bin/g++ g++ /usr/bin/g++-8
+sudo update-alternatives --config gcc-8
+sudo update-alternatives --config gcc
+gcc-8 --version
+gcc --version
