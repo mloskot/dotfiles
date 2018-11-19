cb726cbbf1b899ce71f4ef4adf42d65ac961a24a
Mateusz Łoskot <mateusz@loskot.net>
2018-11-19 17:20:28 +0100

    Switch Boost bootstrap to verioned layout

--- wsl/scripts/boost-bootstrap-for-gil.sh
+++ wsl/scripts/boost-bootstrap-for-gil.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -x
+
+if [ ! -f boost-build.jam ]; then
+    echo "Run inside Boost root"
+    exit 1
+fi
+
 ./bootstrap.sh
-./b2 headers
-./b2 --with-test --with-filesystem variant=debug stage
-./b2 --with-test --with-filesystem variant=release stage
+b2 headers
+
+#./b2 --with-test --with-filesystem variant=debug stage
+#./b2 --with-test --with-filesystem variant=release stage
+b2 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
