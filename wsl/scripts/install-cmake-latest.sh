7a6cd735043295e011d27d1b74e185881a3bd5d2
Mateusz Łoskot <mateusz@loskot.net>
2018-11-28 22:39:06 +0100

    Bump CMake to 3.13.1

--- wsl/scripts/install-cmake-latest.sh
+++ wsl/scripts/install-cmake-latest.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -x
+version='3.13.1'
 
 sudo rm -rf /usr/local/bin/cmake*
 sudo rm -rf /usr/local/bin/ctest*
@@ -9,6 +10,6 @@ sudo rm -rf /usr/local/man/man1/ctest*
 sudo rm -rf /usr/local/man/man7/cmake*
 sudo rm -rf /usr/local/share/cmake*
 
-wget -O cmake-linux.sh https://cmake.org/files/v3.13/cmake-3.13.0-Linux-x86_64.sh
+wget -O cmake-linux.sh https://cmake.org/files/v3.13/cmake-${version}-Linux-x86_64.sh
 sudo sh cmake-linux.sh -- --skip-license --prefix=/usr/local
 cmake --version
