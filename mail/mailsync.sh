ae4686eea945c73cd41f1b7cb40d36dc803a55ba
Mateusz Łoskot <mateusz@loskot.net>
2019-02-26 12:34:22 +0100

    Fix .offlineimap.log path

--- mail/mailsync.sh
+++ mail/mailsync.sh
@@ -7,7 +7,7 @@
 imapserver=imap.gmail.com
 imapactive=$(ps -ef | grep '[o]fflineimap' | wc -l)
 netactive=$(ping -c3 $imapserver >/dev/null 2>&1 && echo up || echo down)
-mailsync="/usr/local/bin/offlineimap -o -q -u quiet -l ~/.offlineimap.log"
+mailsync="/usr/local/bin/offlineimap -o -q -u quiet -l /home/mloskot/.offlineimap.log"
 
 # kill offlineimap if active, sometimes it hangs
 case $imapactive in
