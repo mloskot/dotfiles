623465542f6c91ba109a257079408d1ff6478fa6
Mateusz Łoskot <mateusz@loskot.net>
2019-05-03 22:08:41 +0200

    Scrape CMake version from download site

--- scripts/install-cmake-latest.ps1
+++ scripts/install-cmake-latest.ps1
@@ -1,49 +1,73 @@
 #!/usr/bin/env powershell
 #Requires -RunAsAdministrator
 
-$version = '3.14.2'
+$downloadUrl = 'https://cmake.org/download/'
+$downloadSite = Invoke-WebRequest $downloadUrl
 
-$major, $minor, $patch = $version.split('.');
+$latestVersion = ($downloadSite.AllElements | `
+    Where {$_.tagName -match 'H3' -and $_.innerText -match "Latest Release" } | `
+    Select-Object -ExpandProperty InnerText).Trim();
+if ($latestVersion -match 'Latest Release\s+\((\d+\.\d+\.\d+)\)') {
+    $latestVersion = $matches[1]
+} else {
+    Write-Host ('Failed to extract version of latest release from {0}' -f $downloadUrl);
+    exit 1;
+}
+
+$major, $minor, $patch = $latestVersion.split('.');
 try {
-  $currentVersion = (cmake --version 2>&1);
-  $currentVersion = $currentVersion.split(' ')[2] # cmake version X.Y.Z
+    if (where cmake) {
+        $currentVersion = (cmake --version 2>&1);
+    } else {
+        $cmakeExe = (Join-Path $env:ProgramFiles 'CMake\bin\cmake')
+        Write-Host ("Checking version of '{0}'" -f $cmakeExe)
+        $currentVersion = (&"$cmakeExe" --version 2>&1);
+    }
+    $currentVersion = $currentVersion.split(' ')[2] # cmake version X.Y.Z
 } catch {
-  $currentVersion = $null;
+    $currentVersion = $null;
 }
 if ($null -ne $currentVersion) {
-  $currentMajor, $currentMinor, $currentPatch = $currentVersion.split('.');
-  $currentPatch = $currentPatch.split('-')[0] # 0-rcX to 0
-  $patch = $patch.split('-')[0]
-  if ([Version]::new($currentMajor, $currentMinor, $currentPatch) -ge `
-      [Version]::new($major, $minor, $patch)) {
-        Write-Host ('Installed CMake {0} is not newer than {1}' -f $currentVersion.ToString(), $version);
+    $currentMajor, $currentMinor, $currentPatch = $currentVersion.split('.');
+    $currentPatch = $currentPatch.split('-')[0] # 0-rcX to 0
+    $patch = $patch.split('-')[0]
+    if ([Version]::new($currentMajor, $currentMinor, $currentPatch) -ge `
+        [Version]::new($major, $minor, $patch)) {
+        Write-Host ('Installed CMake {0} is not newer than {1}' -f $currentVersion.ToString(), $latestVersion);
         exit 0;
-  }
+    }
 }
 
-$msi = ('cmake-{0}-win64-x64.msi' -f $version);
+$msi = ('cmake-{0}-win64-x64.msi' -f $latestVersion);
 $out = ('{0}\{1}'-f $PSScriptRoot, $msi);
 if (-not (Test-Path -Path $out -PathType Leaf)) {
-  Write-Host ('Downloading CMake {0}' -f $version)
-  $url = ('https://cmake.org/files/v{0}.{1}/{2}' -f $major, $minor, $msi);
-  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-  (New-Object System.Net.WebClient).DownloadFile($url, $out)
+    Write-Host ('Downloading CMake {0}' -f $latestVersion)
+    $url = ('https://cmake.org/files/v{0}.{1}/{2}' -f $major, $minor, $msi);
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    (New-Object System.Net.WebClient).DownloadFile($url, $out)
 }
 
 $options = @(
-  '/qn',
-  '/norestart',
-  'ADD_CMAKE_TO_PATH="System"'
+    '/qn',
+    '/norestart',
+    'ADD_CMAKE_TO_PATH="System"'
 );
 $options = ($options.GetEnumerator() | % { ('{0}' -f $_) }) -join ' '
 try {
-  Write-Host ("Installing {0}" -f $out)
-  $p = Start-Process -Wait -PassThru -FilePath $out -Args $options
-  Write-Host ("Installer exited with code {0}" -f $p.ExitCode)
+    Write-Host ("Installing {0}" -f $out)
+    $p = Start-Process -Wait -PassThru -FilePath $out -Args $options
+    Write-Host ("Installer exited with code {0}" -f $p.ExitCode)
 } catch {
-  Write-Host $_.Exception.Message
-  exit 1
+    Write-Host $_.Exception.Message
+    exit 1
 }
 Remove-Item -Path $out -Force
 
-cmake --version
+if (where cmake) {
+    cmake --version
+} else {
+    Write-Host ("Re-launch console to update PATH")
+    $cmakeExe = (Join-Path $env:ProgramFiles 'CMake\bin\cmake')
+    Write-Host ("Checking version of '{0}'" -f $cmakeExe)
+    &"$cmakeExe" --version
+}
