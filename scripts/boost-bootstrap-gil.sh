61f9937a671158282b865050247f6a846d9200c8
Mateusz Łoskot <mateusz@loskot.net>
2019-07-16 15:43:13 +0200

    Bootstrap Boost using multiple Linux toolsets

--- scripts/boost-bootstrap-gil.sh
+++ scripts/boost-bootstrap-gil.sh
@@ -9,5 +9,7 @@ fi
 ./bootstrap.sh
 b2 -j8 headers
 
-b2 toolset=gcc variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
-b2 toolset=clang variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
+b2 -j8 toolset=gcc-5 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
+b2 -j8 toolset=gcc-9 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
+b2 -j8 toolset=clang-6 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
+b2 -j8 toolset=clang-8 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
