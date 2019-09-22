334f317b9cac860cdf607d82eede243e4602f1bc
Mateusz Łoskot <mateusz@loskot.net>
2019-09-22 22:54:23 +0200

    Fix CMake versioned folder look-up

--- scripts/install-cmake-latest.sh
+++ scripts/install-cmake-latest.sh
@@ -3,29 +3,39 @@ set -e
 
 url="https://cmake.org/download/"
 version=$(curl --silent $url | grep -oP "<h3>Latest Release\s+\(\K(\d+\.\d+\.\d+)")
-current_version=$(cmake --version | grep version | grep -oP ".+\K(\d+\.\d+\.\d+)")
 
 is_newer()
 {
     test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1";
 }
 
-if ! is_newer $version $current_version; then
-    echo "Installed CMake $current_version is not newer than $version"
-    exit 0
+if type cmake > /dev/null 2>&1; then
+    current_version=$(cmake --version | grep version | grep -oP ".+\K(\d+\.\d+\.\d+)")
+    if ! is_newer $version $current_version; then
+        echo "Installed CMake $current_version is not older than available $version"
+        exit 0
+    else
+        echo "Installed CMake $current_version is older than available $version"
+    fi
+else
+    echo "No CMake installed"
 fi
 
-echo "Downloading CMake $version"
-curl --silent -o cmake-linux.sh https://cmake.org/files/v3.14/cmake-${version}-Linux-x86_64.sh
+version_folder=v$(echo $version | grep -oP "\d+\.\d+")
+url="https://cmake.org/files/${version_folder}/cmake-${version}-Linux-x86_64.sh"
+echo "Downloading $url"
+curl --silent -o cmake-linux.sh $url
 
-echo "Uninstalling CMake $current_version"
-sudo rm -rf /usr/local/bin/cmake*
-sudo rm -rf /usr/local/bin/ctest*
-sudo rm -rf /usr/local/doc/cmake
-sudo rm -rf /usr/local/man/man1/cmake*
-sudo rm -rf /usr/local/man/man1/ctest*
-sudo rm -rf /usr/local/man/man7/cmake*
-sudo rm -rf /usr/local/share/cmake*
+if type cmake > /dev/null 2>&1; then
+    echo "Uninstalling CMake $current_version"
+    sudo rm -rf /usr/local/bin/cmake*
+    sudo rm -rf /usr/local/bin/ctest*
+    sudo rm -rf /usr/local/doc/cmake
+    sudo rm -rf /usr/local/man/man1/cmake*
+    sudo rm -rf /usr/local/man/man1/ctest*
+    sudo rm -rf /usr/local/man/man7/cmake*
+    sudo rm -rf /usr/local/share/cmake*
+fi
 
 echo "Installing CMake $version"
 sudo sh cmake-linux.sh -- --skip-license --prefix=/usr/local
