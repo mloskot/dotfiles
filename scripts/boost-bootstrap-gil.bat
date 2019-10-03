ec431732a59d306c72b1da771a4410a8fe56b096
Mateusz Łoskot <mateusz@loskot.net>
2019-10-03 18:33:02 +0200

    Add --with-system to Boost bootstrap

--- scripts/boost-bootstrap-gil.bat
+++ scripts/boost-bootstrap-gil.bat
@@ -9,5 +9,5 @@ call .\bootstrap.bat
 .\b2 -j8 headers
 
 rem .\b2 -j8 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
-.\b2 -j8 toolset=msvc-14.1 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
-.\b2 -j8 toolset=msvc-14.2 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
+.\b2 -j8 toolset=msvc-14.1 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem --with-system stage
+.\b2 -j8 toolset=msvc-14.2 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem --with-system stage
