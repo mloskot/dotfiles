935a7ece2f938d9eca71990c56b12beacbfaaab5
Mateusz Łoskot <mateusz@loskot.net>
2020-01-07 18:04:49 +0100

    Add scripts/install-gcc-all.sh

---
+++ scripts/install-gcc-all.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#set -x
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get -y update
+
+for v in 5 6 7 8 9
+do
+    sudo apt-get -y install gcc-${v} g++-${v}
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${v} 60 --slave /usr/bin/g++ g++ /usr/bin/g++-${v}
+    sudo update-alternatives --config gcc-${v}
+    sudo update-alternatives --config gcc
+    gcc-${v} --version
+    gcc --version
+done
