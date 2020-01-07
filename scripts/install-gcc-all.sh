93bd4d03ab28febd6776751a232813897fc15d84
Mateusz Łoskot <mateusz@loskot.net>
2020-01-07 18:13:49 +0100

    Fix update-alternatives for GCC

--- scripts/install-gcc-all.sh
+++ scripts/install-gcc-all.sh
@@ -6,9 +6,11 @@ sudo apt-get -y update
 for v in 5 6 7 8 9
 do
     sudo apt-get -y install gcc-${v} g++-${v}
-    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${v} 60 --slave /usr/bin/g++ g++ /usr/bin/g++-${v}
-    sudo update-alternatives --config gcc-${v}
-    sudo update-alternatives --config gcc
     gcc-${v} --version
-    gcc --version
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${v} 60 --slave /usr/bin/g++ g++ /usr/bin/g++-${v}
 done
+
+sudo update-alternatives --config gcc-9
+sudo update-alternatives --config gcc
+
+gcc --version
