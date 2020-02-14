8f5283ab1c87489474aa536b481a3bbbc4f2f8b2
Mateusz Łoskot <mateusz@loskot.net>
2020-02-14 20:39:16 +0100

    b2 no longer needs -j

--- scripts/boost-bootstrap-gil.sh
+++ scripts/boost-bootstrap-gil.sh
@@ -7,9 +7,9 @@ if [ ! -f boost-build.jam ]; then
 fi
 
 ./bootstrap.sh
-b2 -j8 headers
+b2 headers
 
-b2 -j8 toolset=gcc-5 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
-b2 -j8 toolset=gcc-9 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
-b2 -j8 toolset=clang-6 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
-b2 -j8 toolset=clang-9 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
+b2 toolset=gcc-5 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
+b2 toolset=gcc-9 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
+b2 toolset=clang-6 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
+b2 toolset=clang-9 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
