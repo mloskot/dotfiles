16b9f547829840e969db3cbe5f219023234e4ab8
Mateusz Łoskot <mateusz@loskot.net>
2020-03-26 23:05:37 +0100

    Remove GIL dependency on Boost.Test

--- scripts/boost-bootstrap-gil.bat
+++ scripts/boost-bootstrap-gil.bat
@@ -8,8 +8,7 @@ if not exist boost-build.jam (
 call .\bootstrap.bat
 .\b2 headers
 
-rem .\b2 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem stage
-.\b2 toolset=msvc-14.1 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem --with-system stage
-.\b2 toolset=msvc-14.2 variant=debug,release address-model=64 --layout=versioned --with-test --with-filesystem --with-system stage
+rem .\b2 toolset=msvc-14.1 variant=debug,release address-model=64 --layout=versioned --with-filesystem stage
+.\b2 toolset=msvc-14.2 variant=debug,release address-model=64 --layout=versioned --with-filesystem stage
 
 rmdir /s /q .\bin.v2
