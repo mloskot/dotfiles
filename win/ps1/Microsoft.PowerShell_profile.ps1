0fdc2411974022d15a70d2f3ae5c58e07e67c974
Mateusz Łoskot <mateusz@loskot.net>
2020-09-29 20:44:08 +0200

    PS1: Update profile settings

--- win/ps1/Microsoft.PowerShell_profile.ps1
+++ win/ps1/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,9 @@
-$global:DefaultUser = [System.Environment]::UserName
+# PowerShell 5: $HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+# PowerShell 7: $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+ $global:DefaultUser = [System.Environment]::UserName
 
 # Set l and ls alias to use the new Get-ChildItemColor cmdlets
+Set-Alias l Get-ChildItemColorFormatWide -Option AllScope
 Set-Alias ll Get-ChildItemColor -Option AllScope
 Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
 
@@ -8,7 +11,13 @@ Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
 function cuserprofile { Set-Location ~ }
 Set-Alias ~ cuserprofile -Option AllScope
 
-Import-Module Get-ChildItemColor
+# Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
+# Install-Module posh-git -Scope CurrentUser
+# Install-Module oh-my-posh -Scope CurrentUser
 Import-Module posh-git
 Import-Module oh-my-posh
+
+# Install-Module -AllowClobber Get-ChildItemColor
+Import-Module Get-ChildItemColor
+
 Set-Theme Paradox
