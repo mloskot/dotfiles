c343be25b7f74b17a172e6303a82e7dc1a5c546c
Mateusz Łoskot <mateusz@loskot.net>
2021-04-22 16:24:59 +0200

    Install certbot with pip

--- scripts/install-certbot.sh
+++ scripts/install-certbot.sh
@@ -1,8 +1,9 @@
-#/bin/sh
-sudo apt-get -y update
-sudo apt-get -y install software-properties-common
-sudo add-apt-repository -y universe
-sudo add-apt-repository -y ppa:certbot/certbot
-sudo apt-get -y update
-sudo apt-get -y install certbot
+#!/bin/bash
+# WARNING: Do not install with snap as systemd/snapd is broken on WSL
+sudo apt update
+sudo apt install python3 python3-venv libaugeas0
+sudo python3 -m venv /opt/certbot/
+sudo /opt/certbot/bin/pip install --upgrade pip
+sudo /opt/certbot/bin/pip install certbot
+sudo ln -s /opt/certbot/bin/certbot /usr/bin/certbot
 certbot --version
