277a73ed3abd9934fc612673098764b91054c592
Mateusz Łoskot <mateusz@loskot.net>
2022-06-25 14:41:59 +0200

    Add install-lazygit.sh

---
+++ scripts/install-lazygit.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -x
+
+if [ ! -x /usr/bin/lsb_release ]; then
+    echo "Linux expected"
+    exit 1
+fi
+
+# Source https://lindevs.com/install-lazygit-on-ubuntu/
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
+sudo rm -f lazygit.tar.gz
+lazygit --version
