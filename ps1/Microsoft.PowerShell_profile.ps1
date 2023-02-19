b90f2f75cd290b95ad0083d54e7ae8ed905cc7ee
Mateusz Łoskot <mateusz@loskot.net>
2023-02-20 00:43:53 +0100

    ps1: Switch to powerline-go

--- ps1/Microsoft.PowerShell_profile.ps1
+++ ps1/Microsoft.PowerShell_profile.ps1
@@ -15,12 +15,6 @@ Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
 function cuserprofile { Set-Location ~ }
 Set-Alias ~ cuserprofile -Option AllScope
 
-# Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
-# Install-Module posh-git -Scope CurrentUser
-Import-Module posh-git
-# Install-Module oh-my-posh -Scope CurrentUser
-Import-Module oh-my-posh
-
 # Install-Module -AllowClobber Get-ChildItemColor
 Import-Module Get-ChildItemColor
 
@@ -33,14 +27,23 @@ else {
     Write-Warning ("Cannot find '{0}'" -f $rgPath)
 }
 
-# Select oh-my-posh theme
-if($env:TERM_PROGRAM -eq 'vscode') {
-    #Set-PoshPrompt -Theme robbyrussel
-    #Set-PoshPrompt -Theme star
-    Set-PoshPrompt -Theme marcduiker
-    Set-PoshPrompt -Theme C:\Users\mateuszl\.theme-marcduiker-mloskot.omp.json
-} else {
-    #Set-PoshPrompt -Theme marcduiker
-    #Set-PoshPrompt -Theme star
-    Set-PoshPrompt -Theme C:\Users\mateuszl\.theme-marcduiker-mloskot.omp.json
+# Load powerline-go prompt
+$env:VIRTUAL_ENV_DISABLE_PROMPT=$true
+function global:prompt {
+    $pwd = $ExecutionContext.SessionState.Path.CurrentLocation
+    $startInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $startInfo.FileName = "powerline-go"
+    $startInfo.Arguments = "-shell bare -hostname-only-if-ssh -trim-ad-domain -cwd-mode plain -newline"
+    $startInfo.Environment["TERM"] = "xterm-256color"
+    $startInfo.CreateNoWindow = $true
+    $startInfo.StandardOutputEncoding = [System.Text.Encoding]::UTF8
+    $startInfo.RedirectStandardOutput = $true
+    $startInfo.UseShellExecute = $false
+    $startInfo.WorkingDirectory = $pwd
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $startInfo
+    $process.Start() | Out-Null
+    $standardOut = $process.StandardOutput.ReadToEnd()
+    $process.WaitForExit()
+    $standardOut
 }
--- ps1/Microsoft.VSCode_profile.ps1
+++ ps1/Microsoft.VSCode_profile.ps1
@@ -1,6 +1,9 @@
 # PowerShell 5: $HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 # PowerShell 7: $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
 # PowerShell VS Code Integrated Console: C:\Users\mateuszl\Documents\PowerShell\Microsoft.VSCode_profile.ps1
+if (-not (Test-Path env:PSModulePath)) {
+    return
+}
 $global:DefaultUser = [System.Environment]::UserName
 
 # Set l and ls alias to use the new Get-ChildItemColor cmdlets
@@ -11,12 +14,6 @@ Set-Alias ls Get-ChildItemColorFormatWide -Option AllScope
 # Helper function to set location to the User Profile directory
 function cuserprofile { Set-Location ~ }
 Set-Alias ~ cuserprofile -Option AllScope
-
-# Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck    
-# Install-Module posh-git -Scope CurrentUser
-Import-Module posh-git
-# Install-Module oh-my-posh -Scope CurrentUser
-Import-Module oh-my-posh
 
 # Install-Module -AllowClobber Get-ChildItemColor
 Import-Module Get-ChildItemColor
@@ -30,4 +27,23 @@ else {
     Write-Warning ("Cannot find '{0}'" -f $rgPath)
 }
 
-Set-PoshPrompt -Theme C:\Users\mateuszl\.theme-marcduiker-mloskot.omp.json
+# Load powerline-go prompt
+$env:VIRTUAL_ENV_DISABLE_PROMPT=$true
+function global:prompt {
+    $pwd = $ExecutionContext.SessionState.Path.CurrentLocation
+    $startInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $startInfo.FileName = "powerline-go"
+    $startInfo.Arguments = "-shell bare -hostname-only-if-ssh -trim-ad-domain -cwd-mode plain -newline"
+    $startInfo.Environment["TERM"] = "xterm-256color"
+    $startInfo.CreateNoWindow = $true
+    $startInfo.StandardOutputEncoding = [System.Text.Encoding]::UTF8
+    $startInfo.RedirectStandardOutput = $true
+    $startInfo.UseShellExecute = $false
+    $startInfo.WorkingDirectory = $pwd
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $startInfo
+    $process.Start() | Out-Null
+    $standardOut = $process.StandardOutput.ReadToEnd()
+    $process.WaitForExit()
+    $standardOut
+}
