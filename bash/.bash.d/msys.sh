63dfd8607ca43875ad17184884d669ab13d39787
Mateusz Łoskot <mateusz@loskot.net>
2023-04-16 21:13:57 +0200

    bash: Disable MSYS_NO_PATHCONV as it causes issues

--- bash/.bash.d/msys.sh
+++ bash/.bash.d/msys.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if  [[ "$(uname)" =~ "MINGW64_NT" ]]; then
+if  [[ "$(uname)" =~ "XXXMINGW64_NT" ]]; then
     echo "Setting MSYS_NO_PATHCONV=1 to resolve Azure CLI issues on Windows 10"
     export MSYS_NO_PATHCONV=1
 fi
