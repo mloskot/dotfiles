6472c3b16ea5f2a4eb8f74c85adf9af678ad60b1
Mateusz Łoskot <mateusz@loskot.net>
2023-06-06 13:49:46 +0200

    bash: Fix Go bin path

--- bash/.bash.d/go.sh
+++ bash/.bash.d/go.sh
@@ -4,9 +4,12 @@ if [[ -d /usr/local/go/bin ]]; then
     export PATH=$PATH:/usr/local/go/bin
 fi
 
+if [[ -d $HOME/go/bin ]]; then
+    PATH=$PATH:$HOME/go/bin
+    export PATH
+fi
+
 if [[ -z "$GOPATH" ]]; then
     GOPATH="$HOME/go"
     export GOPATH
 fi
-PATH=$PATH:$GOPATH/bin
-export PATH
