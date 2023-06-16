f1275da40e05fbcffb80b0c0d5e5149e3aec15ed
Mateusz Łoskot <mateusz@loskot.net>
2023-06-16 21:52:52 +0200

    bash: Add install.sh

---
+++ bash/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for f in .bash*
+do
+    if [[ -L ${HOME}/${f} ]]; then
+        echo "Deleting symlink ${HOME}/${f}"
+        rm "${HOME}/${f}"
+    fi
+    if [[ -f ${HOME}/${f} ]]; then
+        echo "Backing up ${HOME}/${f} to ${HOME}/${f}.original"
+        mv -f "${HOME}/${f}" "${HOME}/${f}.original"
+    fi
+    echo "Symlinking ${PWD}/${f} to ${HOME}/${f}"
+    ln -s "${PWD}/${f}" "${HOME}/${f}"
+done
