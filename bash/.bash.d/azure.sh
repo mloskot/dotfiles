a6b20b3d53fc1b5ddb5d17a4235351cfd72c5b8d
Mateusz Łoskot <mateusz@loskot.net>
2023-11-24 14:52:35 +0100

    bash: Better check for az and gh

--- bash/.bash.d/azure.sh
+++ bash/.bash.d/azure.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+if ! command -v "az" >/dev/null; then
+    return 
+fi
 
-# Azure CLI
 alias a='az'
 alias ak='az aks'
 alias aks='az aks show --query "{Name:name,Status:provisioningState,Power:powerState.code}"'
--- bash/.bash.d/github.sh
+++ bash/.bash.d/github.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+if ! command -v "gh" >/dev/null; then
+    return 
+fi
 
-# GitHub CLI
+eval "$(gh completion -s bash)"
+
 alias hpc='gh pr create -f'
 alias hpl='gh pr list'
 alias hpo='gh pr checkout'
@@ -8,7 +12,3 @@ alias hrl='gh run list'
 alias hrv='gh run view'
 alias hwl='gh workflow list'
 alias hwr='gh workflow run'
-
-if command -v "gh" >/dev/null; then
-    eval "$(gh completion -s bash)"
-fi
