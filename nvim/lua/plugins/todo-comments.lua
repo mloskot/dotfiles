f069a8c84925016f463dfc434c98d67456886cbc
Mateusz Łoskot <mateusz@loskot.net>
2023-12-21 23:11:21 +0100

    nvim: Simplify todo-comments

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -2,11 +2,5 @@ return {
     "folke/todo-comments.nvim",
     dependencies = "nvim-lua/plenary.nvim",
     lazy = false,
-    config = function()
-      require("todo-comments").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
+    config = true
 }
