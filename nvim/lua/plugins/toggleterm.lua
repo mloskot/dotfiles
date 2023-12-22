2e9e836292feecba77b3f00a15e39989a4349832
Mateusz Łoskot <mateusz@loskot.net>
2023-12-22 03:07:24 +0100

    nvim: Disable unused mappings for toggleterm

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,11 +1,11 @@
 function _G.set_terminal_keymaps()
   local opts = { buffer = 0 }
   vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', 'jj', [[<C-\><C-n>]], opts)
+  --vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  --vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  --vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  --vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
   vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 end
 
