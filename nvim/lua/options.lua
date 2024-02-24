5d0f655892ef47aa656a5c8e30cceba340fc538d
Mateusz Łoskot <mateusz@loskot.net>
2024-02-24 06:14:00 +0100

    Tidy up

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,28 +1,21 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.opt.guicursor = ""
-
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
+vim.opt.colorcolumn = "80"
 vim.opt.expandtab = true
-vim.opt.softtabstop=4
-vim.opt.shiftwidth=4
-vim.opt.tabstop=4
-vim.opt.smartindent = true
-vim.opt.wrap = true
-
-vim.opt.swapfile = true
-
+vim.opt.guicursor = ""
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
+vim.opt.nu = true
+vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
-
-vim.opt.colorcolumn = "80"
+vim.opt.shiftwidth=4
+vim.opt.smartindent = true
+vim.opt.softtabstop=4
+vim.opt.swapfile = false
+vim.opt.tabstop=4
+vim.opt.termguicolors = true
+vim.opt.wrap = true
 
 -- https://github.com/NvChad/NvChad/issues/1526#issuecomment-1234637754
 if vim.fn.has('win32') == 1 then
