07b201e7edb9be4bc09b9bbabddf9c74616d7075
Mateusz Łoskot <mateusz@loskot.net>
2024-02-24 06:13:48 +0100

    nvim: Add install script

---
+++ nvim/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ -L ${HOME}/.config/nvim ]]; then
+    echo "Deleting symlink ${HOME}/.config/nvim"
+    rm "${HOME}/.config/nvim"
+fi
+echo "Symlinking ${PWD}/nvim to ${HOME}/.config/nvim"
+ln -s "${PWD}/nvim" "${HOME}/.config/"
