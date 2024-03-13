f39ecb8f7d5e82f8cfcf91b026df8831aafea69e
Mateusz Łoskot <mateusz@loskot.net>
2024-03-13 10:37:43 +0100

    winget: List more essentials
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- scripts/winget-upgrade-essentials.ps1
+++ scripts/winget-upgrade-essentials.ps1
@@ -1,18 +1,25 @@
 $packages = @(
     'Amazon.Corretto.18',
     'BurntSushi.ripgrep.MSVC',
+    'dandavison.delta',
     'Git.Git',
     'GitHub.cli',
     'GitHub.GitHubDesktop',
     'GnuPG.GnuPG',
+    'GnuPG.Gpg4win',
     'GoLang.Go',
     'Hashicorp.Packer',
     'junegunn.fzf',
     'Kitware.CMake',
     'Kubernetes.kubectl',
     'Microsoft.AzureCLI',
+    'Microsoft.WinDbg',
     'MikeFarah.yq',
     'Neovim.Neovim',
+    'sharkdp.bat',
+    'sharkdp.fd',
+    'sharkdp.hyperfine',
+    'sharkdp.pastel',
     'stedolan.jq',
     ''
 )
@@ -23,6 +30,6 @@ foreach ($packageId in $packages) {
     if (-not $packageId) {
         continue
     }
-    Write-Host "Upgrading package $packageId" -ForegroundColor DarkGreen
-    & winget upgrade --id $packageId
+    Write-Host "Installing or upgrading package $packageId" -ForegroundColor DarkGreen
+    & winget install --id $packageId
 }
