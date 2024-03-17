2a7dd35cbc24446987b7b8612c603ff6be3f67c1
Mateusz Łoskot <mateusz@loskot.net>
2024-03-17 01:10:21 +0100

    sesh: Complete dotfiles.sh

--- sesh/scripts/azure-hosting.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-tmux rename-window .dotfiles
-tmux new-window
-tmux rename-window lazygit
-tmux send-keys "lazygit" Enter
--- sesh/scripts/dotfiles.sh
+++ sesh/scripts/dotfiles.sh
@@ -1,1 +1,5 @@
-#!/bin/bash
+tmux rename-window bash
+tmux new-window
+tmux rename-window lazygit
+tmux send-keys "lazygit" Enter
+tmux select-window -p
