1dbb2aea14ea1430093e278512e55bf3a0903a9a
Mateusz Łoskot <mateusz@loskot.net>
2024-03-20 13:09:29 +0100

    sesh: Add sessions
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- sesh/scripts/azure-hosting.sh
+++ sesh/scripts/azure-hosting.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+tmux rename-window git
+tmux send-keys "cd ~/azure-hosting/azure-hosting-gitops && direnv allow" Enter
+tmux send-keys "lazygit" Enter
+
+tmux new-window
+tmux rename-window gitops
+tmux send-keys "cd ~/azure-hosting/azure-hosting-gitops && direnv allow" Enter
+
+tmux new-window
+tmux rename-window cluster-dev
+tmux send-keys "cd ~/azure-hosting/azure-hosting-gitops/clusters/dev && direnv allow" Enter
+
+tmux new-window
+tmux rename-window cluster-stg
+tmux send-keys "cd ~/azure-hosting/azure-hosting-gitops/clusters/stg && direnv allow" Enter
+
+tmux new-window
+tmux rename-window cluster-prd
+tmux send-keys "cd ~/azure-hosting/azure-hosting-gitops/clusters/prd && direnv allow" Enter
+
+tmux new-window
+tmux rename-window inventory
+tmux send-keys "cd ~/azure-hosting/azure-hosting-inventory" Enter
+
+tmux new-window
+tmux rename-window docs
+tmux send-keys "cd ~/azure-hosting/azure-hosting-docs" Enter
+
+tmux select-window -t 0
