cc6bafd1c6a8593297c6b659c01d853d363a6226
Mateusz Łoskot <mateusz@loskot.net>
2024-05-29 13:30:04 +0200

    bash: Add starship-sesh-atuin installer
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- scripts/install-essentials.sh
+++ scripts/install-essentials.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
+set -e
 set -x
 
 sudo apt-get update
-sudo apt-get install autoconf automake build-essential gcc gdb git libtool valgrind
+sudo apt-get install git tmux
+
+sudo rm -rf /opt/go
+rm -f go.tar.gz
+curl -o go.tar.gz https://dl.google.com/go/go1.22.3.linux-amd64.tar.gz
+sudo tar -C /opt -xzf go.tar.gz
+rm -f go.tar.gz
+
+curl -sS https://starship.rs/install.sh | sh
+
+curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh | bash
+
+go install github.com/joshmedeski/sesh@latest
