f0845b7781b426cc5a2eb4c654e9cb3317069cfa
Mateusz Łoskot <mateusz@loskot.net>
2024-07-09 18:36:38 +0200

    Add zoxide to essential installs
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- scripts/install-essentials.sh
+++ scripts/install-essentials.sh
@@ -7,12 +7,14 @@ sudo apt-get install git tmux
 
 sudo rm -rf /opt/go
 rm -f go.tar.gz
-curl -o go.tar.gz https://dl.google.com/go/go1.22.3.linux-amd64.tar.gz
+curl -o go.tar.gz https://dl.google.com/go/go1.22.5.linux-amd64.tar.gz
 sudo tar -C /opt -xzf go.tar.gz
 rm -f go.tar.gz
 
 curl -sS https://starship.rs/install.sh | sh
 
 curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh | bash
+
+curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 
 go install github.com/joshmedeski/sesh@latest
