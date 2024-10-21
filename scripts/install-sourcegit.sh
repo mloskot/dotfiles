48f63f30721869dabd34d55b23c3858ae6e7ac4a
Mateusz Łoskot <mateusz@loskot.net>
2024-10-21 12:44:21 +0200

    scripts: Add SourceGit installer
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

---
+++ scripts/install-sourcegit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -x
+
+if [ ! -x /usr/bin/lsb_release ]; then
+    echo "Linux expected"
+    exit 1
+fi
+
+# Source https://lindevs.com/install-lazygit-on-ubuntu/
+SOURCEGIT_VERSION=$(curl -s "https://api.github.com/repos/sourcegit-scm/sourcegit/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+curl -Lo sourcegit.deb "https://github.com/sourcegit-scm/sourcegit/releases/latest/download/sourcegit_${SOURCEGIT_VERSION}-1_amd64.deb"
+sudo dpkg -i sourcegit.deb
+sudo rm -f sourcegit.deb
