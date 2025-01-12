b9406ea90a1291bb3b2d4cda82415a58c0e8632b
Mateusz Łoskot <mateusz@loskot.net>
2025-01-12 22:11:57 +0100

    Add install-ko.sh
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- scripts/install-k9s.sh
+++ scripts/install-k9s.sh
@@ -6,7 +6,6 @@ if [ ! -x /usr/bin/lsb_release ]; then
     exit 1
 fi
 
-
 K9S_VERSION=$(curl -s "https://api.github.com/repos/derailed/k9s/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
 echo "Downloading K9s ${K9S_VERSION}"
 curl -Lo k9s.deb "https://github.com/derailed/k9s/releases/latest/download/k9s_linux_amd64.deb"
