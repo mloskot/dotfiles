f7215ccd67d5a92906b676f28ed7d7ad872c9777
Mateusz Łoskot <mateusz@loskot.net>
2025-02-01 13:01:36 +0100

    feat: Install latest Go, not hard-coded version
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- scripts/install-golang.sh
+++ scripts/install-golang.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 set -x
-MY_GO_VERSION=1.23.2
 
 if [ ! -x /usr/bin/lsb_release ]; then
     echo "Linux expected"
     exit 1
 fi
 
-if ! go version | grep "${MY_GO_VERSION}" ; then
+VERSION=$(curl -s https://go.dev/VERSION?m=text | head -n 1)
+
+if ! go version | grep "${VERSION}" ; then
     sudo rm -rf /opt/go
     rm -f go.tar.gz
-    curl -o go.tar.gz "https://dl.google.com/go/go${MY_GO_VERSION}.linux-amd64.tar.gz"
+    curl -o go.tar.gz "https://dl.google.com/go/${VERSION}.linux-amd64.tar.gz"
     sudo tar -C /opt -xzf go.tar.gz
     rm -f go.tar.gz
 fi
+
+go version
