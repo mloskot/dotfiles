9befacb840688d4c3681287face81640ef6bbf54
Mateusz Łoskot <mateusz@loskot.net>
2025-02-01 22:10:06 +0100

    feat: Add installer for go-task tool
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

---
+++ scripts/install-gotask.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -x
+
+if [ ! -x /usr/bin/lsb_release ]; then
+    echo "Linux expected"
+    exit 1
+fi
+
+VERSION=$(curl -s "https://api.github.com/repos/go-task/task/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+
+curl -Lo task.deb "https://github.com/go-task/task/releases/download/v${VERSION}/task_linux_amd64.deb"
+sudo dpkg -i task.deb
+rm -f task.deb
