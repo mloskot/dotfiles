9dd24468fcec021c96d29694888e9d21c697ea5c
Mateusz Łoskot <mateusz@loskot.net>
2025-02-04 22:41:50 +0100

    feat: Configure catppuccin theme for K9s
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- scripts/install-k9s.sh
+++ scripts/install-k9s.sh
@@ -12,3 +12,7 @@ curl -Lo k9s.deb "https://github.com/derailed/k9s/releases/latest/download/k9s_l
 sudo dpkg -i k9s.deb
 sudo rm -f k9s.deb
 k9s version
+
+K9S_SKINS="${XDG_CONFIG_HOME:-$HOME/.config}/k9s/skins"
+mkdir -p "$K9S_SKINS"
+curl -L https://github.com/catppuccin/k9s/archive/main.tar.gz | tar xz -C "$K9S_SKINS" --strip-components=2 k9s-main/dist
