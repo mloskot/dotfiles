9005e30e0946a406b64a03d97ec3cc9fb3bc4384
Mateusz Łoskot <mateusz@loskot.net>
2025-02-05 00:26:03 +0100

    feat: Neovim installer
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

---
+++ scripts/install-neovim.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -x
+
+if [ ! -x /usr/bin/lsb_release ]; then
+    echo "Linux expected"
+    exit 1
+fi
+
+VERSION=$(curl -s "https://api.github.com/repos/neovim/neovim/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+
+curl -LO "https://github.com/neovim/neovim/releases/download/v${VERSION}/nvim-linux-x86_64.tar.gz"
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+rm -f nvim-linux-x86_64.tar.gz
+
+which nvim
+nvim --version
