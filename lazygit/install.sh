98c8b855daf2cfc6c5ff316ced426bb31fb1d362
Mateusz Łoskot <mateusz@loskot.net>
2025-02-07 16:35:51 +0100

    fix: save existing lazygit config
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- lazygit/install.sh
+++ lazygit/install.sh
@@ -5,5 +5,8 @@ if [[ -L "${target}" ]]; then
     echo "Deleting symlink ${target}"
     rm "${target}"
 fi
+if [[ -f "${target}" ]]; then
+    mv "${target}" "${target}.original"
+fi
 echo "Symlinking ${PWD}/config.yml to ${target}"
 ln -s "${PWD}/config.yml" "${target}"
