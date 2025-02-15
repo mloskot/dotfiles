b2e7ce06110079b127eaf35876ed3d94d37c4983
Mateusz Łoskot <mateusz@loskot.net>
2025-02-15 23:41:43 +0100

    feat(sesh): Manage AZURE_CONFIG_DIR for Azure CLI-based sessions

--- sesh/scripts/cadcorp-cloud.sh
+++ sesh/scripts/cadcorp-cloud.sh
@@ -2,6 +2,20 @@
 cc_env=$1
 az_tenant_id=$(az account show --query homeTenantId --output tsv)
 
+# Force Azure CLI with configuration path per single environment
+# https://learn.microsoft.com/en-us/cli/azure/azure-cli-configuration#cli-configuration-file
+AZURE_CONFIG_DIR="${HOME}/.azure-${cc_env}"
+#export AZURE_CONFIG_DIR
+tmux set-environment AZURE_CONFIG_DIR "${AZURE_CONFIG_DIR}"
+
+CC_ENV="${cc_env}"
+#export CC_ENV
+tmux set-environment CC_ENV "${CC_ENV}"
+
+# Force update environment in window 0
+tmux send-keys -t 0 "export AZURE_CONFIG_DIR=${AZURE_CONFIG_DIR}" Enter
+tmux send-keys -t 0 "export CC_ENV=${CC_ENV}" Enter
+# Reconfigure Window 0 created by sesh
 tmux rename-window command
 tmux send-keys "cd ~/cadcorp-cloud" Enter
 
