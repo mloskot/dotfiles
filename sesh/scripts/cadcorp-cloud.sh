48dd711f7ea0d83feb3a83f360db1ee54187e38b
Mateusz Łoskot <mateusz@loskot.net>
2025-02-16 00:16:43 +0100

    fix(sesh): Ensure AZURE_CONFIG_DIR is set before az command invoked

--- sesh/scripts/cadcorp-cloud.sh
+++ sesh/scripts/cadcorp-cloud.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 cc_env=$1
-az_tenant_id=$(az account show --query homeTenantId --output tsv)
 
 # Force Azure CLI with configuration path per single environment
 # https://learn.microsoft.com/en-us/cli/azure/azure-cli-configuration#cli-configuration-file
-AZURE_CONFIG_DIR="${HOME}/.azure-${cc_env}"
-#export AZURE_CONFIG_DIR
-tmux set-environment AZURE_CONFIG_DIR "${AZURE_CONFIG_DIR}"
+AZURE_CONFIG_DIR="${HOME}/.azure-${CC_ENV}"
+export AZURE_CONFIG_DIR
 
 CC_ENV="${cc_env}"
 #export CC_ENV
+az_tenant_id=$(az account show --query homeTenantId --output tsv)
+if [[ -z "${az_tenant_id}" ]]; then
+  echo "az_tenant_id is empty"
+  exit 1
+fi
+
+# Set required session-local environment variables
+tmux set-environment AZURE_CONFIG_DIR "${AZURE_CONFIG_DIR}"
 tmux set-environment CC_ENV "${CC_ENV}"
 
-# Force update environment in window 0
+# Force environment update in window 0
 tmux send-keys -t 0 "export AZURE_CONFIG_DIR=${AZURE_CONFIG_DIR}" Enter
 tmux send-keys -t 0 "export CC_ENV=${CC_ENV}" Enter
-# Reconfigure Window 0 created by sesh
+# Configure Window 0 created by sesh
 tmux rename-window command
 tmux send-keys "cd ~/cadcorp-cloud" Enter
 
