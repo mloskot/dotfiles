3d5f492e927049c9733a671b7923d074808244c2
Mateusz Łoskot <mateusz@loskot.net>
2025-02-28 19:11:19 +0100

    fix: fzf autocompletion was disabled
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- bash/.bash.d/fzf.sh
+++ bash/.bash.d/fzf.sh
@@ -3,9 +3,14 @@ if ! command -v "fzf" >/dev/null; then
     return
 fi
 
-# Prefer binding CTRL+R for Atuin, if it is available
-if command -v "atuin" >/dev/null; then
-    return
+if command -v "fd" >/dev/null; then
+    export FZF_DEFAULT_COMMAND='fd --type f'
 fi
 
+#FZF_CTRL_T_COMMAND= FZF_ALT_C_COMMAND=
 eval "$(fzf --bash)"
+
+# Prefer binding CTRL+R for Atuin, if it is available
+if command -v "atuin" >/dev/null; then
+    bind -x '"\C-r": __atuin_history'
+fi
