0039a7ac98f10c8214847d6ba8325ce5b3e79bde
Mateusz Łoskot <mateusz@loskot.net>
2025-03-14 11:21:50 +0100

    fix: Use official zen.desktop
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- scripts/install/zen.sh
+++ scripts/install/zen.sh
@@ -19,21 +19,7 @@ curl -Lo zen.tar.xz "https://github.com/zen-browser/desktop/releases/download/${
 sudo tar -C /opt -xf zen.tar.xz
 rm -f zen.tar.xz
 
-sudo tee "${HOME}/.local/share/applications/zen.desktop" << EOF > /dev/null
-[Desktop Entry]
-Version=1.0
-Name=Zen Browser
-Comment=Experience tranquillity while browsing the web without people tracking you!
-GenericName=Web Browser
-Categories=GNOME;GTK;Network;WebBrowser;
-Keywords=Internet;WWW;Browser;Web;Explorer
-TryExec=/opt/zen/zen
-Icon=/opt/zen/browser/chrome/icons/default/default128.png
-Terminal=false
-Type=Application
-X-MultipleArgs=false
-MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ftp;x-scheme-handler/chrome;video/webm;application/x-xpinstall;
-StartupNotify=true
-EOF
+curl -Lo "${HOME}/.local/share/applications/zen.desktop" \
+  https://raw.githubusercontent.com/zen-browser/desktop/refs/heads/dev/AppDir/zen.desktop
 
 zen --version
