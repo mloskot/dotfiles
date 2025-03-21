4651ed6b40a2912ead77f1d6963e84a7429e6348
Mateusz Łoskot <mateusz@loskot.net>
2025-03-21 21:48:55 +0100

    fix: Back up Lazygit config if exists
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- install/terminal/app-lazygit.sh
+++ install/terminal/app-lazygit.sh
@@ -4,6 +4,10 @@ set -e
 # shellcheck disable=SC1090
 source ~/.dotfiles/log.sh "${BASH_SOURCE[0]}"
 
+# shellcheck disable=SC1090
+source ~/.dotfiles/backup.sh
+backup_file ~/.config/lazygit/config.yml
+
 VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
 echolog "Installing lazygit ${VERSION}"
 
@@ -13,7 +17,8 @@ sudo rm -f lazygit.tar.gz
 
 lazygit --version
 
-echolog "Installing ~/.config/lazygit"
-[[ ! -L ~/.config/lazygit ]] && ln -s ~/.dotfiles/config/lazygit ~/.config/lazygit
+echolog "Installing ~/.config/lazygit/config.yml"
+mkdir -p ~/.config/lazygit
+[[ ! -L ~/.config/lazygit/config.yml ]] && ln -s ~/.dotfiles/config/lazygit/.yml ~/.config/lazygit/config.yml
 
 [[ "${BASH_SOURCE[0]}" != "${0}" ]] && return 0
