58a1affe4977098e6f09ed80ef5f57e1c9fcc729
Mateusz Łoskot <mateusz@loskot.net>
2025-04-18 21:56:07 +0200

    feat: more Gnome settings
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- install/desktop/set-gnome-settings.sh
+++ install/desktop/set-gnome-settings.sh
@@ -7,5 +7,12 @@ source ~/.dotfiles/log.sh "${BASH_SOURCE[0]}"
 echolog "Center new windows in the middle of the screen"
 gsettings set org.gnome.mutter center-new-windows true
 
-echolog "Reveal week numbers in the Gnome calendar"
+echolog "Hide Home and Trash icons from desktop"
+gsettings set org.gnome.shell.extensions.ding show-home false
+gsettings set org.gnome.shell.extensions.ding show-trash false
+
+echolog "Show week numbers in the Gnome calendar"
 gsettings set org.gnome.desktop.calendar show-weekdate true
+
+echolog "Show battery percentage"
+gsettings set org.gnome.desktop.interface show-battery-percentage true
