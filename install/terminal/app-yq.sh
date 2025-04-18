376d18e53c6cf57223c9047cf1a0944ce7715376
Mateusz Łoskot <mateusz@loskot.net>
2025-04-18 10:58:48 +0200

    feat: add yq installer
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

---
+++ install/terminal/app-yq.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# shellcheck disable=SC1090
+source ~/.dotfiles/log.sh "${BASH_SOURCE[0]}"
+
+VERSION=$(curl -s "https://api.github.com/repos/mikefarah/yq/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+echolog "Installing yq ${VERSION}"
+
+curl -Lo yq.tar.gz "https://github.com/mikefarah/yq/releases/download/v${VERSION}/yq_linux_amd64.tar.gz"
+sudo tar -xf yq.tar.gz -C /usr/local/bin --transform='s/yq_linux_amd64/yq/g'
+sudo rm -f yq.tar.gz
+
+yq --version
+
+[[ "${BASH_SOURCE[0]}" != "${0}" ]] && return 0
