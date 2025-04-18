acc387322e8319caff111eadfe8f22ba2fd48573
Mateusz Łoskot <mateusz@loskot.net>
2025-04-18 23:11:26 +0200

    fix: Ensure atuin is in PATH during installation

--- install/terminal/app-atuin.sh
+++ install/terminal/app-atuin.sh
@@ -7,9 +7,12 @@ source ~/.dotfiles/log.sh "${BASH_SOURCE[0]}"
 echolog "Installing atuin"
 curl --proto '=https' --tlsv1.2 -sSf https://setup.atuin.sh | bash
 
-echolog "Installing bash-preexec"
-curl https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh -o ~/.bash-preexec.sh
+PATH=$HOME/.atuin/bin
+export PATH
 
 atuin --version
+
+echolog "Installing bash-preexec"
+curl https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh -o ~/.bash-preexec.sh
 
 [[ "${BASH_SOURCE[0]}" != "${0}" ]] && return 0
