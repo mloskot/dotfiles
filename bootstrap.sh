8ecc1fe00a4e0655873134588a6b62db117f57e3
Mateusz Łoskot <mateusz@loskot.net>
2025-04-18 22:52:01 +0200

    fix: undefined variable in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,11 +12,11 @@ sudo apt-get install -y git >/dev/null
 echo "Cloning mloskot/dotfiles..."
 rm -rf ~/.dotfiles
 git clone https://github.com/mloskot/dotfiles.git ~/.dotfiles >/dev/null
-if [[ "${DOTFILES_REF}" != "main" ]]; then
-    echo "Switching from main branch to ${DOTFILES_REF}"
-	cd ~/.dotfiles || exit 1
-	git fetch origin "${DOTFILES_REF}" && git checkout "${DOTFILES_REF}"
-	cd - || exit 1
+if [[ "${DOTFILES_REF:-main}" != "main" ]]; then
+  echo "Switching from main branch to ${DOTFILES_REF}"
+  cd ~/.dotfiles || exit 1
+  git fetch origin "${DOTFILES_REF}" && git checkout "${DOTFILES_REF}"
+  cd - || exit 1
 fi
 
 echo "Installation starting..."
