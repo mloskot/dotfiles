9defe7271f351c502ffacd2ef8aa7ae4a09130aa
Mateusz Łoskot <mateusz@loskot.net>
2025-04-20 16:27:17 +0200

    feat: Configure text and fractional scaling
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- install/desktop/set-gnome-settings.sh
+++ install/desktop/set-gnome-settings.sh
@@ -4,6 +4,12 @@ set -e
 # shellcheck disable=SC1090
 source ~/.dotfiles/log.sh "${BASH_SOURCE[0]}"
 
+echolog "Enable fractional scaling"
+gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
+
+echo "Enable text scaling"
+gsettings set org.gnome.desktop.interface text-scaling-factor 1.2
+
 echolog "Auto-hide the Dock"
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
 gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
