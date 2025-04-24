bb48c3c9a60e928073e86d78f894e3373eeb2758
Mateusz Łoskot <mateusz@loskot.net>
2025-04-24 09:14:59 +0200

    WIP: non-verifed - 2025-04-24T09:14:59

---
+++ install/desktop/app-azure-storage-explorer.sh
@@ -0,0 +1,13 @@
+[Desktop Entry]
+Name=Azure Storage Explorer
+Comment=Manage your Azure cloud storage resources from your desktop
+Exec=/opt/azure-storage-explorer/StorageExplorer
+Icon=/opt/azure-storage-explorer/resources/app/out/app/icon.png
+Type=Application
+Categories=FileManager;FileTransfer;Network;
+StartupNotify=true
+Terminal=false
+X-MultipleArgs=false
+Keywords=Azure;Storage;Explorer;File;
+X-Desktop-File-Install-Version=0.27
+
