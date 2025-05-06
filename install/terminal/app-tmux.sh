bc7c13013e323d708341d7e64869b10dcacaac3c
Mateusz Łoskot <mateusz@loskot.net>
2025-05-06 14:00:20 +0200

    fix: install tmux plugins
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- install/terminal/app-tmux.sh
+++ install/terminal/app-tmux.sh
@@ -20,8 +20,11 @@ tmux -V
 echolog "Installing ~/.tmux.conf"
 [[ ! -L ~/.tmux.conf ]] && ln -s ~/.dotfiles/config/tmux/dot-tmux.conf ~/.tmux.conf
 
-echolog "Installing tmux plugin tpm"
+echolog "Installing tmux plugin manager (tpm)"
 rm -rf ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echolog "Installing tmux plugins"
+~/.tmux/plugins/tpm/bin/install_plugins
 
 [[ "${BASH_SOURCE[0]}" != "${0}" ]] && return 0
