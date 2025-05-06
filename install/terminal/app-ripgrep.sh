3a582cadc4a1885c34664759198d930bfd96a714
Mateusz Łoskot <mateusz@loskot.net>
2025-05-06 16:13:34 +0200

    feat: ripgrep installer
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

---
+++ install/terminal/app-ripgrep.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+# shellcheck disable=SC1090
+source ~/.dotfiles/log.sh "${BASH_SOURCE[0]}"
+
+echolog "Installing ripgrep"
+
+VERSION=$(curl -s "https://api.github.com/repos/BurntSushi/ripgrep/releases/latest" | grep -Po '"tag_name": "\K[0-9.]+')
+echolog "Downloading ripgrep ${VERSION}"
+
+curl -Lo ripgrep.deb "https://github.com/BurntSushi/ripgrep/releases/download/${VERSION}/ripgrep_${VERSION}-1_amd64.deb"
+sudo dpkg -i ripgrep.deb
+rm -f ripgrep.deb
+
+rg --version
+
+[[ "${BASH_SOURCE[0]}" != "${0}" ]] && return 0
