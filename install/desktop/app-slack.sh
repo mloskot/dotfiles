96809cba95f98ecb9c06804f876e0b07a0f2c64b
Mateusz Łoskot <mateusz@loskot.net>
2025-05-14 23:37:02 +0200

    feat: installer for Slack from deb
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

---
+++ install/desktop/app-slack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+# shellcheck disable=SC1090
+source ~/.dotfiles/log.sh "${BASH_SOURCE[0]}"
+
+echolog "Installing Slack"
+
+VERSION=$(curl -s https://slack.com/intl/en-gb/downloads/linux | grep -Po 'Version \K[0-9.]+')
+echolog "Downloading Slack ${VERSION}"
+curl -Lo slack.deb "https://downloads.slack-edge.com/desktop-releases/linux/x64/${VERSION}/slack-desktop-${VERSION}-amd64.deb"
+sudo dpkg -i slack.deb
+rm -f slack.deb
+
+[[ "${BASH_SOURCE[0]}" != "${0}" ]] && return 0
