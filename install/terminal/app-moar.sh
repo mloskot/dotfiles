d42d4c7e3168177484f3c1828833252f717eb6bc
Mateusz Łoskot <mateusz@loskot.net>
2025-05-17 16:58:19 +0200

    feat(terminal): Moar installer
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

---
+++ install/terminal/app-moar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+# shellcheck disable=SC1090
+source ~/.dotfiles/log.sh "${BASH_SOURCE[0]}"
+
+echolog "Installing moar"
+
+VERSION=$(curl -s "https://api.github.com/repos/walles/moar/releases/latest" | grep -Po '"tag_name":\s*"v?\K[0-9.]+')
+echolog "Downloading moar ${VERSION}"
+
+curl -Lo moar "https://github.com/walles/moar/releases/download/v${VERSION}/moar-v${VERSION}-linux-amd64"
+mv moar ~/.local/bin
+chmod +x ~/.local/bin/moar
+
+echolog "Installed $(which moar) $(moar --version)"
+
+[[ "${BASH_SOURCE[0]}" != "${0}" ]] && return 0
