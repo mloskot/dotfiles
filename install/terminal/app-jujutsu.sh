461d5093ec0ceddeffbfdaafbab0d666bd88e33c
Mateusz Łoskot <mateusz@loskot.net>
2025-05-18 01:44:01 +0200

    feat(terminal): Add jujutsu installer
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

---
+++ install/terminal/app-jujutsu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+# shellcheck disable=SC1090
+source ~/.dotfiles/log.sh "${BASH_SOURCE[0]}"
+
+echolog "Installing jj"
+
+# shellcheck disable=SC1090
+source ~/.dotfiles/backup.sh
+backup_file ~/.config/jj
+
+VERSION=$(curl -s "https://api.github.com/repos/jj-vcs/jj/releases/latest" | grep -Po '"tag_name":\s*"v?\K[0-9.]+')
+echolog "Downloading jj ${VERSION}"
+
+curl -Lo jj.tar.gz "https://github.com/jj-vcs/jj/releases/download/v${VERSION}/jj-v${VERSION}-x86_64-unknown-linux-musl.tar.gz"
+tar -xzf jj.tar.gz --directory ~/.local/bin ./jj
+rm -f jj.tar.gz
+
+echolog "Installed $(which jj) $(jj --version)"
+
+echolog "Installing ~/.config/jj"
+[[ ! -L ~/.config/jj ]] && ln -s ~/.dotfiles/config/jj ~/.config/jj
+
+[[ "${BASH_SOURCE[0]}" != "${0}" ]] && return 0
