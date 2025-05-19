74c11eb7c8d665a0e77b8b5d2bcb4a504a1a3ed0
Mateusz Łoskot <mateusz@loskot.net>
2025-05-19 14:53:07 +0200

    feat(git): More aliases for GitHub CLI commands
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- config/bash/dot-bash.d/github.sh
+++ config/bash/dot-bash.d/github.sh
@@ -5,9 +5,13 @@ fi
 
 eval "$(gh completion -s bash)"
 
+alias hil='gh issue list'
+alias his='gh issue status'
+alias hiv='gh issue view'
 alias hpc='gh pr create -f'
 alias hpl='gh pr list'
 alias hpo='gh pr checkout'
+alias hps='gh pr checkout' # s for switch like in git switch, git sw
 alias hrl='gh run list'
 alias hrv='gh run view'
 alias hwl='gh workflow list'
