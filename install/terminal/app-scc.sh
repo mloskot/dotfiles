a30d7bc54ebc5b3147134d6234e16580889374da
Mateusz Łoskot <mateusz@loskot.net>
2025-05-24 16:58:04 +0200

    feat(terminal): Add installer for sloc cloc and code
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

---
+++ install/terminal/app-scc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+# shellcheck disable=SC1090
+source ~/.dotfiles/log.sh "${BASH_SOURCE[0]}"
+
+echolog "Installing scc (sloc cloc and code)"
+
+VERSION=$(curl -s "https://api.github.com/repos/boyter/scc/releases/latest" | grep -Po '"tag_name":\s*"v?\K[0-9.]+')
+echolog "Downloading scc ${VERSION}"
+
+curl -Lo scc.tar.gz "https://github.com/boyter/scc/releases/download/v${VERSION}/scc_Linux_x86_64.tar.gz"
+sudo tar -xzf scc.tar.gz --directory ~/.local/bin scc
+rm -f scc.tar.gz
+
+echolog "Installed $(which scc) $(scc --version)"
+
+[[ "${BASH_SOURCE[0]}" != "${0}" ]] && return 0
