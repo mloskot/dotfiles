4a6b306f13e80f1370237491d8efed769637bd9c
Mateusz Łoskot <mateusz@loskot.net>
2025-05-30 23:34:33 +0200

    feat(terminal): Add installer for FLux CLI
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

---
+++ install/terminal/app-flux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+# shellcheck disable=SC1090
+source ~/.dotfiles/log.sh "${BASH_SOURCE[0]}"
+
+echolog "Installing Flux CLI ${FLUX_VERSION}"
+
+if [[ -z "${FLUX_VERSION}" ]];  then
+  echolog "FLUX_VERSION not set, installing latest"
+else
+  echolog "FLUX_VERSION not set, installing latest"
+fi
+
+curl -s https://fluxcd.io/install.sh | FLUX_VERSION="${FLUX_VERSION}" bash -s ~/.local/bin
+
+
+echolog "Installed $(which flux) $(flux version --client)"
+
+[[ "${BASH_SOURCE[0]}" != "${0}" ]] && return 0
