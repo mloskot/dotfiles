671524470f3a525891322b654236f73f530c34b5
Mateusz Łoskot <mateusz@loskot.net>
2025-07-27 19:42:12 +0200

    feat(helm): Skip installation if latest version found
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- install/terminal/app-helm.sh
+++ install/terminal/app-helm.sh
@@ -2,12 +2,21 @@
 source ~/.dotfiles/err.sh "${BASH_SOURCE[0]}"
 source ~/.dotfiles/log.sh "${BASH_SOURCE[0]}"
 
-echolog "Installing Helm (latest)"
+VERSION=$(curl -s "https://api.github.com/repos/helm/helm/releases/latest" | grep -Po '"tag_name":\s*"v?\K[0-9.]+')
 
-HELM_INSTALL_DIR=~/.local/bin
-export HELM_INSTALL_DIR
-curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-export -n HELM_INSTALL_DIR
+if command -v "helm" &>/dev/null; then
+  HELM_VERSION="$(helm version --short 2>/dev/null)"
+  echolog "Found existing Helm ${HELM_VERSION}"
+fi
+
+if [[ ! $HELM_VERSION =~ $VERSION ]]; then
+  echolog "Installing Helm ${VERSION}"
+
+  HELM_INSTALL_DIR=~/.local/bin
+  export HELM_INSTALL_DIR
+  curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+  export -n HELM_INSTALL_DIR
+fi
 
 installed_cmd="$(command -v helm)"
 installed_ver="$(helm version)"
