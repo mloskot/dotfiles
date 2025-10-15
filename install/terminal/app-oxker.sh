c9c3fa1bc7cbcf89a01acc21f84a997a4a1376df
Mateusz Łoskot <mateusz@loskot.net>
2025-10-15 19:11:53 +0200

    fix(terminal): Missing version info from oxker installer

--- install/terminal/app-oxker.sh
+++ install/terminal/app-oxker.sh
@@ -4,6 +4,9 @@ source ~/.dotfiles/log.sh "${BASH_SOURCE[0]}"
 
 echolog "Installing oxker"
 
+VERSION=$(curl -s "https://api.github.com/repos/mrjackwills/oxker/releases/latest" | grep -Po '"tag_name":\s*"v?\K[0-9.]+')
+echolog "Downloading oxker ${VERSION}"
+
 curl https://raw.githubusercontent.com/mrjackwills/oxker/main/install.sh | bash
 
 installed_cmd="$(command -v oxker)"
