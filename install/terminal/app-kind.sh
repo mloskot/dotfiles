642dcc99cc41a0313162a976ed3e034aef38e9b5
Mateusz Łoskot <mateusz@loskot.net>
2025-11-13 23:05:09 +0100

    feat(teraminal): Add kind installer
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

---
+++ install/terminal/app-kind.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+source ~/.dotfiles/err.sh "${BASH_SOURCE[0]}"
+source ~/.dotfiles/log.sh "${BASH_SOURCE[0]}"
+
+echolog "Installing kind"
+
+VERSION=$(curl -s "https://api.github.com/repos/kubernetes-sigs/kind/releases/latest" | grep -Po '"tag_name":\s*"v?\K[0-9.]+')
+if [[ -x ~/.local/bin/kind ]]; then
+  if ~/.local/bin/kind --version | grep "${VERSION}" ; then
+    echolog "kind ${VERSION} already installed"
+    ~/.local/bin/kind --version
+    VERSION=""
+  fi
+fi
+
+if [[ -n "${VERSION}" ]]; then
+  echolog "Downloading kind ${VERSION}"
+  mkdir -p ~/.local/bin
+  curl -Lo ~/.local/bin/kind "https://github.com/kubernetes-sigs/kind/releases/download/v${VERSION}/kind-linux-amd64"
+  chmod +x ~/.local/bin/kind
+fi
+
+installed_cmd="$(command -v kind)"
+installed_ver="$(kind --version)"
+echolog "Installed ${installed_cmd} ${installed_ver}"
