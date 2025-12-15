cf458b9f5983ee380649cf809be239e4eb974beb
Mateusz Łoskot <mateusz@loskot.net>
2025-12-15 21:21:02 +0100

    feat: Add installer for Helm chart-testing tool
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

---
+++ install/terminal/app-helm-ct.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+source ~/.dotfiles/err.sh "${BASH_SOURCE[0]}"
+source ~/.dotfiles/log.sh "${BASH_SOURCE[0]}"
+
+VERSION=$(curl -s "https://api.github.com/repos/helm/chart-testing/releases/latest" | grep -Po '"tag_name":\s*"v?\K[0-9.]+')
+
+if command -v "ct" &>/dev/null; then
+  CT_VERSION="$(ct version 2>/dev/null | grep -Po "Version.+v\K[0-9.]+")"
+  echolog "Found existing ct ${HELM_VERSION}"
+else
+  # Install ct dependencies
+  sudo apt install -y python3-yamale yamllint
+fi
+
+if [[ ! $CT_VERSION =~ $VERSION ]]; then
+  echolog "Installing ct ${VERSION}"
+
+  curl -Lo chart-testing.tar.gz "https://github.com/helm/chart-testing/releases/download/v${VERSION}/chart-testing_${VERSION}_linux_amd64.tar.gz"
+  tar -xzf chart-testing.tar.gz --directory ~/.local/bin ct
+  mkdir ~/.ct
+  sudo tar -xzf chart-testing.tar.gz --directory ~/.ct etc
+  rm -f chart-testing.tar.gz
+fi
+
+installed_cmd="$(command -v ct)"
+installed_ver="$(ct version)"
+echolog "Installed ${installed_cmd} ${installed_ver}"
