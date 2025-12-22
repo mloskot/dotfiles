46ec2319c30c40136cb4688f216ff4cc01e8742b
Mateusz Łoskot <mateusz@loskot.net>
2025-12-23 00:33:47 +0100

    chore(terminal): Catch up with new go-task naming
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- install/terminal/app-cruise.sh
+++ install/terminal/app-cruise.sh
@@ -4,10 +4,10 @@ source ~/.dotfiles/log.sh "${BASH_SOURCE[0]}"
 
 echolog "Installing cruise"
 
-VERSION=$(curl -s "https://api.github.com/repos/NucleoFusion/cruise/releases/latest" | grep -Po '"tag_name":\s*"v?\K[0-9.]+')
+VERSION=$(curl -s "https://api.github.com/repos/cruise-org/cruise/releases/latest" | grep -Po '"tag_name":\s*"v?\K[0-9.]+')
 echolog "Downloading cruise ${VERSION}"
 
-curl -Lo cruise.deb "https://github.com/NucleoFusion/cruise/releases/download/v${VERSION}/cruise_${VERSION}_linux_amd64.deb"
+curl -Lo cruise.deb "https://github.com/cruise-org/cruise/releases/download/v${VERSION}/cruise_${VERSION}_linux_amd64.deb"
 sudo dpkg -i cruise.deb
 rm -f cruise.deb
 
--- install/terminal/app-gotask.sh
+++ install/terminal/app-gotask.sh
@@ -7,7 +7,7 @@ echolog "Installing gotask"
 VERSION=$(curl -s "https://api.github.com/repos/go-task/task/releases/latest" | grep -Po '"tag_name":\s*"v?\K[0-9.]+')
 echolog "Downloading gotask ${VERSION}"
 
-curl -Lo task.deb "https://github.com/go-task/task/releases/download/v${VERSION}/task_linux_amd64.deb"
+curl -Lo task.deb "https://github.com/go-task/task/releases/download/v${VERSION}/task_${VERSION}_linux_amd64.deb"
 sudo dpkg -i task.deb
 rm -f task.deb
 
