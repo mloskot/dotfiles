5d4e5c032b8954b5bc047f35b680bcce32e76aa5
Mateusz Łoskot <mateusz@loskot.net>
2025-12-28 09:38:23 +0100

    feat(bash): Add zellij completion

---
+++ config/bash/dot-bash.d/zellij.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if ! command -v "zellij" >/dev/null; then
+    return
+fi
+
+alias zj='zellij'
+
+# Bash completion and aliases from Zellij (i.e. zr, zrf, ze, zef)
+source <(zellij setup --generate-completion bash) >/dev/null 2>&1
