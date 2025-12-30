c9158184f05efa4f53c1527d6f4a23c9e6f5a4a6
Mateusz Łoskot <mateusz@loskot.net>
2025-12-30 14:17:02 +0100

    feat(bash): Quick alias for zellij welcome
    Signed-off-by: Mateusz Łoskot <mateusz@loskot.net>

--- config/bash/dot-bash.d/zellij.sh
+++ config/bash/dot-bash.d/zellij.sh
@@ -4,6 +4,7 @@ if ! command -v "zellij" >/dev/null; then
 fi
 
 alias zj='zellij'
+alias zlw='zellij -l welcome'
 
 # Bash completion and aliases from Zellij (i.e. zr, zrf, ze, zef)
 source <(zellij setup --generate-completion bash) >/dev/null 2>&1
