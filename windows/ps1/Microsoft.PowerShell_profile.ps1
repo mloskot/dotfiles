2ade6f54b5a40c7a65956b1fa267c453a97536d1
Mateusz Łoskot <mateusz@loskot.net>
2026-07-24 19:52:33 +0200

    chore: Update PowerShell profile for Windows

--- windows/ps1/Microsoft.PowerShell_profile.ps1
+++ windows/ps1/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,24 @@
-Invoke-Expression (&starship init powershell)
+if (Get-Command -Name starship -CommandType Application) {
+  starship init powershell | Out-String | Invoke-Expression
+}
 
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
+if (Get-Command -Name zoxide -CommandType Application) {
+  zoxide init powershell | Out-String | Invoke-Expression
+}
+
+if (Get-Command -Name atuin -CommandType Application) {
+  atuin init powershell --disable-up-arrow | Out-String | Invoke-Expression
+}
+
+if (Get-Command -Name mise -CommandType Application) {
+  mise activate pwsh | Out-String | Invoke-Expression
+}
+
+if (Get-Command -Name lazygit -CommandType Application) {
+  New-Alias -Name lg -Value lazygit -Force -Option AllScope
+}
+
+function Invoke-GitCommitWIP {
+  &git add -A && &git commit -m "WIP: $(Get-Date -Format o)" --no-verify
+}
+New-Alias -Name wip -Value Invoke-GitCommitWIP -Force -Option AllScope
