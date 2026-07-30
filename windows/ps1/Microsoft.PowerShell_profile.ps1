7bc33043063dd2d3b4d56e0844a67ef8c5bc6c66
Mateusz Łoskot <mateusz.loskot@necsws.com>
2026-07-30 14:44:03 +0200

    feat: PowerShell 7 simple prompt and alises
    Signed-off-by: Mateusz Łoskot <mateusz.loskot@necsws.com>

--- windows/ps1/Microsoft.PowerShell_profile.ps1
+++ windows/ps1/Microsoft.PowerShell_profile.ps1
@@ -1,24 +1,36 @@
-if (Get-Command -Name starship -CommandType Application) {
+# PowerShell 7 profile
+
+function global:prompt {
+  'PS ' + $(Split-Path -Path $ExecutionContext.SessionState.Path.CurrentLocation -Leaf) + '> '
+}
+
+if (Get-Command -Name starship -CommandType Application -ErrorAction SilentlyContinue) {
   starship init powershell | Out-String | Invoke-Expression
 }
 
-if (Get-Command -Name zoxide -CommandType Application) {
+if (Get-Command -Name zoxide -CommandType Application -ErrorAction SilentlyContinue) {
   zoxide init powershell | Out-String | Invoke-Expression
 }
 
-if (Get-Command -Name atuin -CommandType Application) {
+if (Get-Command -Name atuin -CommandType Application -ErrorAction SilentlyContinue) {
   atuin init powershell --disable-up-arrow | Out-String | Invoke-Expression
 }
 
-if (Get-Command -Name mise -CommandType Application) {
+if (Get-Command -Name mise -CommandType Application -ErrorAction SilentlyContinue) {
   mise activate pwsh | Out-String | Invoke-Expression
 }
 
-if (Get-Command -Name lazygit -CommandType Application) {
-  New-Alias -Name lg -Value lazygit -Force -Option AllScope
+if (Get-Command -Name git -CommandType Application -ErrorAction SilentlyContinue) {
+  function Invoke-GitCommitWIP {
+    &git add -A && &git commit -m "WIP: $(Get-Date -Format o)" --no-verify
+  }
+  New-Alias -Name wip -Value Invoke-GitCommitWIP -Force -Option AllScope
+
+  if (Get-Command -Name lazygit -CommandType Application -ErrorAction SilentlyContinue) {
+    New-Alias -Name lg -Value lazygit -Force -Option AllScope
+  }
 }
 
-function Invoke-GitCommitWIP {
-  &git add -A && &git commit -m "WIP: $(Get-Date -Format o)" --no-verify
+if (Get-Command -Name nerdctl -CommandType Application -ErrorAction SilentlyContinue) {
+  New-Alias -Name docker -Value nerdctl -Force -Option AllScope
 }
-New-Alias -Name wip -Value Invoke-GitCommitWIP -Force -Option AllScope
