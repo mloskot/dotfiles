bab4511945a54d517c6d0dfe668b38f4c2b545e4
Mateusz Loskot <mateusz.loskot@necsws.com>
2026-08-05 14:11:48 +0100

    feat: Add Linux-like eza aliases
    Signed-off-by: Mateusz Loskot <mateusz.loskot@necsws.com>

--- windows/ps1/Microsoft.PowerShell_profile.ps1
+++ windows/ps1/Microsoft.PowerShell_profile.ps1
@@ -31,6 +31,65 @@ if (Get-Command -Name git -CommandType Application -ErrorAction SilentlyContinue
   }
 }
 
+if (Get-Command -Name eza -CommandType Application -ErrorAction SilentlyContinue) {
+  # Linux muscle memory helpers, see config/bash/dot-bash_aliases
+
+  function Invoke-Eza {
+    param ([Parameter(ValueFromRemainingArguments = $true)][string[]]$Args)
+    eza @Args
+  }
+  New-Alias -Name ls -Value Invoke-Eza -Force -Option AllScope
+
+  function Invoke-EzaL {
+    param ([Parameter(ValueFromRemainingArguments = $true)][string[]]$Args)
+    eza -lbF --git @Args
+  }
+  New-Alias -Name l -Value Invoke-EzaL -Force -Option AllScope
+
+  function Invoke-EzaLL {
+    param ([Parameter(ValueFromRemainingArguments = $true)][string[]]$Args)
+    eza -lbhgUmu --time-style=long-iso --git --color-scale @Args
+  }
+  New-Alias -Name ll -Value Invoke-EzaLL -Force -Option AllScope
+
+  function Invoke-EzaLA {
+    param ([Parameter(ValueFromRemainingArguments = $true)][string[]]$Args)
+    eza -lbhgUmu --all --time-style=long-iso --git --color-scale @Args
+  }
+  New-Alias -Name la -Value Invoke-EzaLA -Force -Option AllScope
+
+  function Invoke-EzaLX {
+    param ([Parameter(ValueFromRemainingArguments = $true)][string[]]$Args)
+    eza -lbhHigUmuS@ --all --time-style=long-iso --git --color-scale @Args
+  }
+  New-Alias -Name lx -Value Invoke-EzaLX -Force -Option AllScope
+
+  function Invoke-EzaLT {
+    param ([Parameter(ValueFromRemainingArguments = $true)][string[]]$Args)
+    eza --tree --git-ignore --all @Args
+  }
+  New-Alias -Name lt -Value Invoke-EzaLT -Force -Option AllScope
+
+  function Invoke-EzaLTD {
+    param ([Parameter(ValueFromRemainingArguments = $true)][string[]]$Args)
+    eza --tree --git-ignore --only-dirs @Args
+  }
+  New-Alias -Name ltd -Value Invoke-EzaLTD -Force -Option AllScope
+}
+
 if (Get-Command -Name nerdctl -CommandType Application -ErrorAction SilentlyContinue) {
   New-Alias -Name docker -Value nerdctl -Force -Option AllScope
 }
+
+# Aliases for basic commands (Linux muscle memory helpers)
+function Invoke-RemoveItemRecurse {
+  param([Parameter(ValueFromRemainingArguments=$true)] $Args)
+  Remove-Item -Recurse @Args
+}
+New-Alias -Name rmr -Value Invoke-RemoveItemRecurse -Force -Option AllScope
+
+function Invoke-RemoveItemRecurseForce {
+  param([Parameter(ValueFromRemainingArguments=$true)] $Args)
+  Remove-Item -Recurse -Force @Args
+}
+New-Alias -Name rmr -Value Invoke-RemoveItemRecurseForce -Force -Option AllScope
